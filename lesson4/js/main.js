02e151bc0f3b54af8e07a000e89221adf5b0e534
losk773 <losk773@gmail.com>
2017-11-11 21:22:43 +0300

    Update Task 1 and Compelete Task 2

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,11 +1,13 @@
-;(() => {
+;
+(() => {
     let taskSumBtn = document.querySelector('.task-btn[data-task="sum"]');
-    
-    let getInputValue = (target) => {
-        let taskBox = target.closest('.task');
-        let inputValue = taskBox.querySelector('.task-input').value;
+    let taskWordBtn = document.querySelector('.task-btn[data-task="word"]');
 
-        return inputValue;
+    let getInputTask = (target) => {
+        let taskBox = target.closest('.task');
+        let taskInput = taskBox.querySelector('.task-input');
+        
+        return taskInput;
     };
     let getResultBox = (target) => {
         let taskBox = target.closest('.task');
@@ -14,14 +16,37 @@
         return resultBox;
     };
     // Задание 1:
-    let task1 = function (e) {
-        let n = getInputValue(e.target);
-        let resultBox = getResultBox(e.target);
+    let task1 = (event) => {
+        let taskInput = getInputTask(event.target);
+        let resultBox = getResultBox(event.target);
         let sum = 0;
-        for (let i = 1; i <= n; i++) {
+        for (let i = 1; i <= taskInput.value; i++) {
             sum += Math.pow(i, 3);
         }
+        taskInput.value = 1;
         resultBox.innerHTML = sum;
     }
+    // Задание 2:
+    let task2 = (event) => {
+        let taskInput = getInputTask(event.target);
+        let stringInput = taskInput.value;
+        let words = stringInput.split(' ');
+        let resultList = getResultBox(event.target);
+
+        resultList.innerHTML = '';
+
+        if (!parseInt(stringInput) && stringInput) {
+            for (let i = 0; i < words.length; i++) {
+                let listItem = document.createElement('li');
+                listItem.className = 'task-result-item';
+                listItem.innerHTML = words[i]
+                resultList.appendChild(listItem);
+                taskInput.value = '';
+            }
+        } else {
+            alert('Вводить можно только слова!!!');
+        }
+    }
     taskSumBtn.addEventListener('click', task1);
+    taskWordBtn.addEventListener('click', task2);
 })();
