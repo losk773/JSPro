9caf69043ef2f4950a0134cd71d37c9dd5b7e0d4
losk773 <losk773@gmail.com>
2017-11-13 20:17:42 +0300

    Complete Task 4

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -2,11 +2,12 @@
 (() => {
     let taskSumBtn = document.querySelector('.task-btn[data-task="sum"]');
     let taskWordBtn = document.querySelector('.task-btn[data-task="word"]');
+    let taskRandomBtn = document.querySelector('.task-btn[data-task="random"]');
 
     let getInputTask = (target) => {
         let taskBox = target.closest('.task');
         let taskInput = taskBox.querySelector('.task-input');
-        
+
         return taskInput;
     };
     let getResultBox = (target) => {
@@ -47,6 +48,35 @@
             alert('Вводить можно только слова!!!');
         }
     }
+    // Задание 3
+    let task3 = (event) => {
+        let taskInput = getInputTask(event.target);
+        let resultList = getResultBox(event.target);
+        let radioInput = document.querySelectorAll('.task .type-radio');
+        console.log(radioInput);
+        let count = parseInt(taskInput.value);
+        let rand = 0;
+        let randomContainer = '';
+        for (let i = 0; i < radioInput.length; i++) {
+            if (radioInput[i].checked && radioInput[i].dataset.type == 'array') {
+                randomContainer = [];
+                for (let k = 0; k < count; k++) {
+                    
+                    randomContainer.push(Math.floor(rand));
+                }
+            } else if (radioInput[i].checked && radioInput[i].dataset.type == 'map') {
+                randomContainer = new Map();
+                for (let k = 0; k < count; k++) {
+                    rand = Math.random() * 10;
+                    randomContainer.set(k, Math.floor(rand));
+                }
+                console.log(randomContainer);
+            }
+        }
+        resultList.innerHTML = randomContainer;
+        taskInput.value = '';
+    };
     taskSumBtn.addEventListener('click', task1);
     taskWordBtn.addEventListener('click', task2);
+    taskRandomBtn.addEventListener('click', task3);
 })();
