7fcc47c45d3933192a9a2e3e5c202d373d10686b
losk773 <losk773@gmail.com>
2017-11-15 01:26:17 +0300

    create UI and task 8 complete

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -29,7 +29,7 @@
         }
 
         taskInput.value = 1;
-        resultBox.innerHTML = sum;
+        resultBox.innerHTML = '<p class="task-result-text">' + sum + '</p>';
     }
     // Задание 2:
     let task2 = (event) => {
@@ -117,44 +117,69 @@
         };
 
         let money = parseFloat(taskInput.value);
-        let rub = Math.floor(money);
-        let cent = Math.round((money - rub) * 100);
-
-        let ostRub = rub % 10;
-        let ostCent = cent % 10;
-
-        resultBox.innerHTML = '';
-
-        // Проверка на рубли
-        if (ostRub === 1 && rub !== 11) {
-            rub += " " + rubObj.one;
-        } else if ((rub >= 10 && rub <= 20) || ostRub === 0 ) {
-            rub += " " + rubObj.many;
-        } else if ((ostRub >= 2 && ostRub <= 4)) {
-            rub += " " + rubObj.other;
-        } else if (ostRub >= 5 && ostRub <= 9) {
-            rub += " " + rubObj.many;
+        if ( money ) {
+            let rub = Math.floor(money);
+            let cent = Math.round((money - rub) * 100);
+    
+            let ostRub = rub % 10;
+            let ostCent = cent % 10;
+    
+            resultBox.innerHTML = '';
+    
+            // Проверка на рубли
+            if (ostRub === 1 && rub !== 11) {
+                rub += " " + rubObj.one;
+            } else if ((rub >= 10 && rub <= 20) || ostRub === 0 ) {
+                rub += " " + rubObj.many;
+            } else if ((ostRub >= 2 && ostRub <= 4)) {
+                rub += " " + rubObj.other;
+            } else if (ostRub >= 5 && ostRub <= 9) {
+                rub += " " + rubObj.many;
+            }
+    
+            //Проверка на копейки
+            if ((ostCent === 1 && ostCent !== 11)) {
+                cent += " " + centObj.one;
+            } else if ((ostCent >= 10 && ostCent <= 20) || ostCent === 0) {
+                cent += " " + centObj.many;
+            } else if ((ostCent >= 2 && ostCent <= 4)) {
+                cent += " " + centObj.other;
+            } else if ((ostCent >= 5 && ostCent <= 9)) {
+                cent += " " + centObj.many;
+            }
+    
+            resultBox.innerHTML = '<p class="task-result-text">' + rub + ' ' + cent + '</p>';
+        } else {
+            alert('Введите корректную сумму денег!!!');
         }
-
-        //Проверка на копейки
-        if ((ostCent === 1 && ostCent !== 11)) {
-            cent += " " + centObj.one;
-        } else if ((ostCent >= 10 && ostCent <= 20) || ostCent === 0) {
-            cent += " " + centObj.many;
-        } else if ((ostCent >= 2 && ostCent <= 4)) {
-            cent += " " + centObj.other;
-        } else if ((ostCent >= 5 && ostCent <= 9)) {
-            cent += " " + centObj.many;
-        }
-
-        resultBox.innerHTML = rub + ' ' + cent;
 
     };
     // Задание 8
     let task8 = (event) => {
         let taskInput = getInputTask(event.target);
         let resultBox = getResultBox(event.target);
-        console.log(taskInput.value);
+        
+        let [dayBday, monthBday] = (taskInput.value).split('.');
+
+        if (parseInt(dayBday) && parseInt(monthBday) && monthBday < 13 && (dayBday > 0 && dayBday <= 31) ) {
+            let dateObj = new Date();
+            let diffMonth = monthBday - (dateObj.getMonth() + 1);
+            let nowYear = dateObj.getFullYear();
+            let res = 0;
+            let bday = 0;
+    
+            if ( diffMonth < 0 ) {
+                bday = new Date((nowYear + 1), monthBday - 1, dayBday);
+            } else {
+                bday = new Date(nowYear, monthBday - 1, dayBday);
+            }
+            res = parseInt((bday - dateObj) / 86400000);
+    
+            resultBox.innerHTML = '<p class="task-result-text">' + res + ' дней' + '</p>';
+        } else {
+            alert('Введите корректную дату!!!');
+        }
+        
     };
 
     taskSumBtn.addEventListener('click', task1);
