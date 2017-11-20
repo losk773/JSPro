51c1d14913b4ef3ced06a9ca64263a3e3a34bcfe
losk7 <6691755ab>
2017-11-20 04:22:43 +0300

    style search

--- livesearch/js/main.js
+++ livesearch/js/main.js
@@ -1,20 +1,32 @@
 var LiveSearch = (() => {
-    var data = ['Москва', 'Минск', 'Молодечно', 'Киев', 'Нью Йорк', 'Амстердам'];
+    var data = ['Москва', 'Минск', 'Рига', 'Киев', 'Нью Йорк', 'Амстердам',
+                'Лондон', 'Прага', 'Будапешт', 'Париж', 'Вильнюс', 'Берлин',
+                'Стокгольм', 'Копенгаген', 'Санкт-Питербург', 'Токио', 'Пекин', 'Гонгконг',
+                'Сидней', 'Сингапур', 'Варшава', 'Белосток', 'Познань', 'Барселона'];
     return {
         search: function() {
             let input = document.querySelector('input[data-type="search"]');
             let elements = document.querySelectorAll('.search-item');
+            let countBox = document.querySelector('.search-count');
 
             input.addEventListener('keyup', function(){
                 let val = input.value.toLowerCase();
+                let count = 0;
                 for(let i = 0; i < data.length; i++) {
                     let pos = data[i].toLowerCase().indexOf(val);
                     if( pos >= 0 && val !== '') {
                         elements[i].classList.add('active');
+                        count++
+                        countBox.classList.add('active');
+                        countBox.innerHTML = 'Найденных элементов: ' + count;
                     } else {
                         elements[i].classList.remove('active');
-                    }
+                    }  
                 }
+                if (count === 0) {
+                    countBox.classList.remove('active');
+                }
+                
             });
         },
         init: function() {
