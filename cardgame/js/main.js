7e98d03f069cba733f02007d2f977d5bb400d958
losk7 <6691755ab>
2017-11-29 00:18:43 +0300

    update cardgame v1.1.0

--- cardgame/js/main.js
+++ cardgame/js/main.js
@@ -1,10 +1,15 @@
 var Game = (function () {
     let cardsArray = [];
     let colors = ['♠', '♣', '♥', '♦'];
-    let values = [6, 7, 8, 9, 10, 'валет', 'дама', 'король', 'туз'];
-    let result = 0;
+    let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'валет', 'дама', 'король', 'туз'];
+    let resultPlayer = 0;
+    let resultComputer = 0;
     let getCardBtn = document.querySelector('.get-card-btn');
     let stopCardBtn = document.querySelector('.stop-card-btn');
+    let playerBoard = document.querySelector('.player-board');
+    let computerBoard = document.querySelector('.computer-board');
+    let resultPlayerBox = document.querySelector('.result-player');
+    let resultComputerBox = document.querySelector('.result-computer');
 
     return {
         generatePackCards: function () {
@@ -23,64 +28,64 @@ var Game = (function () {
             this.createCards();
         },
         shufflePack: function (arr) {
-            arr.sort((a, b) => { return Math.random() - 0.5});
+            arr.sort((a, b) => {
+                return Math.random() - 0.5;
+            });
         },
         createCards: function () {
             let packBox = document.querySelector('.card-pack');
-            let zIndex = 35;
+            let zIndex = 52;
             let leftPos = 0;
             let topPos = 0;
+            
             for (let i = 0; i < cardsArray.length; i++) {
                 let cardElem = document.createElement('div');
                 cardElem.classList.add('card');
-                cardElem.innerHTML = "<span class='value'>" + cardsArray[i].value + "</span>" +
-                    " " +
-                    "<span class='color'>" + cardsArray[i].color + "</span>";
-                
+                cardElem.innerHTML = "<div class='card-content'>" + 
+                                     "<span class='value'>" + cardsArray[i].value + "</span>"
+                                     +" " +
+                                     "<span class='color'>" + cardsArray[i].color + "</span>" +
+                                     "</div>";
                 cardElem.style.zIndex = --zIndex;
-                leftPos += 2;
-                topPos -= 2;
-                cardElem.style.left = leftPos + 'px';
+                topPos -= 1.5;
                 cardElem.style.top = topPos + 'px';
                 packBox.appendChild(cardElem);
             }
         },
-        getCard: function () {
-            let playBoard = document.querySelector('.play-board');
-            let cardItems = Array.from(document.querySelectorAll('.card'));
-            let resultBox = document.querySelector('.result');
-            let card = cardsArray.shift();
-
-            playBoard.appendChild(cardItems.shift());
-
+        getResult: function (value) {
+            let result = 0;
             switch (true) {
-                case (parseInt(card.value) >= 6 || parseInt(card.value) <= 10):
-                    result += parseInt(card.value);
-                    break;
-                case card.value === 'король':
-                    result += 4;
-                    break;
-                case card.value === 'дама':
-                    result += 3;
-                    break;
-                case card.value === 'валет':
-                    result += 2;
-                    break;
-                case card.value === 'туз':
-                    result += parseInt(prompt('Выберите номинал туза 1/11', ''));
-                    break;
+                case (parseInt(value) >= 6 || parseInt(value) <= 10): result += parseInt(value); break;
+                case value === 'король': result += 4; break;
+                case value === 'дама': result += 3; break;
+                case value === 'валет': result += 2; break;
+                case value === 'туз': result += parseInt(prompt('Выберите номинал туза 1/11', '')); break;
             }
-            resultBox.innerText = result;
+
+            return result;
         },
-        stopedGame: function() {
+        stopedGame: function () {
             getCardBtn.setAttribute('disabled', 'true');
             alert('Вы набрали ' + result + ' очков');
         },
         start: function () {
             this.generatePackCards();
+
+            for (let i = 0; i < 4; i++) {
+                let cardItems = Array.from(document.querySelectorAll('.card'));
+                let card = cardsArray.shift();
+                
+                if (i % 2 == 0) {
+                    playerBoard.appendChild(cardItems.shift());
+                    resultPlayer += this.getResult(card.value);
+                    resultPlayerBox.innerText = resultPlayer;
+                } else {
+                    computerBoard.appendChild(cardItems.shift());
+                }
+            }
+
             getCardBtn.addEventListener('click', this.getCard);
             stopCardBtn.addEventListener('click', this.stopedGame);
-
         }
     }
 })();
