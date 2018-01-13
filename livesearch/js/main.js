a10e3eefc032d7c790c470e6c6249c1559b44147
losk7 <losk773@gmail.com>
2018-01-13 15:16:02 +0300

    Изменения работы приложения: [+] добавлен AJAX запрос, для получения городов [+] Использование Promise Object

--- livesearch/js/main.js
+++ livesearch/js/main.js
@@ -1,44 +1,58 @@
 var LiveSearch = (() => {
-    var data = ['Москва', 'Минск', 'Рига', 'Киев', 'Нью Йорк', 'Амстердам',
-                'Лондон', 'Прага', 'Будапешт', 'Париж', 'Вильнюс', 'Берлин',
-                'Стокгольм', 'Копенгаген', 'Санкт-Питербург', 'Токио', 'Пекин', 'Гонгконг',
-                'Сидней', 'Сингапур', 'Варшава', 'Белосток', 'Познань', 'Барселона'];
+    var data = [];
+    let url = 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
     return {
-        search: function() {
+        loadCities: function(url) {
+            return new Promise((resolve, reject) => {
+                let xhr = new XMLHttpRequest();
+                xhr.responseType = 'json';
+                xhr.open('GET', url);
+    
+                xhr.addEventListener('load', () => resolve(xhr));
+                xhr.addEventListener('error', () => reject(xhr));
+    
+                xhr.send();
+            });
+        },
+        showCities: function(xhr) {
+            let listElements = document.querySelector('.search-data-list');
+            for (city of xhr.response) {
+                let item = document.createElement('div');
+                item.classList.add('search-item');
+                item.innerHTML = '<i class="material-icons">check</i>' + city.name;
+                listElements.appendChild(item);
+                data.push(city.name);
+            }
+        },
+        search: function () {
             let input = document.querySelector('input[data-type="search"]');
             let elements = document.querySelectorAll('.search-item');
             let countBox = document.querySelector('.search-count');
 
-            input.addEventListener('keyup', function(){
+            input.addEventListener('keyup', function () {
                 let val = input.value.toLowerCase();
                 let count = 0;
-                for(let i = 0; i < data.length; i++) {
+                for (let i = 0; i < data.length; i++) {
                     let pos = data[i].toLowerCase().indexOf(val);
-                    if( pos >= 0 && val !== '') {
+                    if (pos >= 0 && val !== '') {
                         elements[i].classList.add('active');
                         count++
                         countBox.classList.add('active');
                         countBox.innerHTML = 'Найденных элементов: ' + count;
                     } else {
                         elements[i].classList.remove('active');
-                    }  
+                    }
                 }
                 if (count === 0) {
                     countBox.classList.remove('active');
                 }
-                
+
             });
         },
-        init: function() {
-            let listElements = document.querySelector('.search-data-list');
-            
-            for (let i = 0; i < data.length; i++) {
-                let item = document.createElement('div');
-                item.classList.add('search-item');
-                item.innerHTML = '<i class="material-icons">check</i>' + data[i];
-                listElements.appendChild(item);
-            }
-            this.search();
+        init: function () {
+            this.loadCities(url)
+                .then((xhr) => this.showCities(xhr))
+                .then(() => this.search());
         }
     };
 })();
